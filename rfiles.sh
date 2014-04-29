0647db0515f64552665fd3b1af09bec9b69d2a0e
tunnels <raphaelstlouis@gmail.com>
2014-04-29 19:54:30 -0400

    improved arguments in rfiles, nothing really in now-playing

--- rfiles.sh
+++ rfiles.sh
@@ -8,8 +8,8 @@ if [ ! -d ~/Desktop/shuffle ]; then
 	mkdir ~/Desktop/shuffle
 fi
 
-cd "$2"
-find . -maxdepth 2 -type f | gshuf -n$1 > /tmp/rfiles.txt
+cd "$1"
+find . -maxdepth 2 -type f | gshuf -n$2 > /tmp/rfiles.txt
 files=$(cat /tmp/rfiles.txt)
 cp $files ~/Desktop/shuffle
 
