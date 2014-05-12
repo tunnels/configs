e7b2147fa7a4d995a2cc503c77a655b72954e7a3
tunnels <raphaelstlouis@gmail.com>
2014-05-12 18:37:44 -0400

    updated

--- dupe.sh
+++ dupe.sh
@@ -4,13 +4,19 @@ filename=$1
 
 if [ ${filename: -4} == ".jpg" ]
 then
-	convert $filename temp_file.png;
-	convert temp_file.png $filename
+	convert "$filename" temp_file.png;
+	convert temp_file.png "$filename"
 	rm temp_file.png
 
 elif [ ${filename: -4} == ".png" ];
 then	
-	convert $filename temp_file.jpg;
-	convert temp_file.jpg $filename
+	convert "$filename" temp_file.jpg;
+	convert temp_file.jpg "$filename"
 	rm temp_file.jpg
+
+elif [ ${filename: -4} == ".gif" ]
+then
+    convert "$filename" temp_file.png;
+    convert temp_file.png "$filename"
+    rm temp_file.png
 fi
