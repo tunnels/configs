c8945b43f75bb97b093c68c7a6601c67e3ff596b
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2014-11-29 21:03:17 -0500

    webm script now saves output to desktop

--- webm.sh
+++ webm.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 filename=`basename $1 .mov`
-cd ~/Desktop/$filename/
-ffmpeg -i $1 -c:v libvpx -b:v $2 -crf $3 $filename.webm
-
+ffmpeg -i $1 -c:v libvpx -b:v $2 -crf $3 ~/Desktop/$filename.webm
