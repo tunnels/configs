b6d834a91d32070c1cd6bd54d3a77c07de152825
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2015-04-24 19:56:51 -0400

    now properly handels spaces in filenames

--- rfiles.sh
+++ rfiles.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ -d ~/Desktop/shuffle ]; then
-	rm ~/Desktop/shuffle/*
+if [ -d ~/Desktop/rfiles_shuffle ]; then
+	rm ~/Desktop/rfiles_shuffle/*
 fi
 
-if [ ! -d ~/Desktop/shuffle ]; then
-	mkdir ~/Desktop/shuffle
+if [ ! -d ~/Desktop/rfiles_shuffle ]; then
+	mkdir ~/Desktop/rfiles_shuffle
 fi
 
+IFS=$'\n'
 cd "$1"
 find . -maxdepth 2 -type f | gshuf -n$2 > /tmp/rfiles.txt
-files=$(cat /tmp/rfiles.txt)
-cp $files ~/Desktop/shuffle
+files=$(cat "/tmp/rfiles.txt")
+cp $files ~/Desktop/rfiles_shuffle
 
