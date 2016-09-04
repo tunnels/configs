de099de14c1ecefb6c21675ea45ffe373e9b2f7f
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2016-09-04 16:15:47 -0400

    now an exact match is queried rather than a substring search

--- mpc/random-album.sh
+++ mpc/random-album.sh
@@ -1,1 +1,1 @@
-/usr/local/bin/mpc clear; /usr/local/bin/mpc search album "$(/usr/local/bin/mpc list album | /usr/local/bin/gshuf -n1)" | /usr/local/bin/mpc add && /usr/local/bin/mpc play
+/usr/local/bin/mpc clear; /usr/local/bin/mpc find album "$(/usr/local/bin/mpc list album | /usr/local/bin/gshuf -n1)" | /usr/local/bin/mpc add && /usr/local/bin/mpc play
